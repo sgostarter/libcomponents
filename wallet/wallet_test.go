11476fe41b30c2b78a720776b850be1b79688052
patdz <patdz@ymipro.com>
2022-05-02 13:18:09 +0800

    ut

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -31,6 +31,22 @@ func TestRedisWallet(t *testing.T) {
 	err = locker.TransToWallet(context.Background(), user, "key1", wallet, user, "key1 to wallet")
 	assert.Nil(t, err)
 
+	total, err := locker.GetTotal(context.Background(), user)
+	assert.Nil(t, err)
+	assert.EqualValues(t, 6, total)
+
+	total, err = wallet.GetCoins(context.Background(), user)
+	assert.Nil(t, err)
+	assert.EqualValues(t, 5, total)
+
 	err = wallet.TransToLocker(context.Background(), user, 2, "remark1", locker, user, "keyx")
 	assert.Nil(t, err)
+
+	total, err = locker.GetTotal(context.Background(), user)
+	assert.Nil(t, err)
+	assert.EqualValues(t, 8, total)
+
+	total, err = wallet.GetCoins(context.Background(), user)
+	assert.Nil(t, err)
+	assert.EqualValues(t, 3, total)
 }
